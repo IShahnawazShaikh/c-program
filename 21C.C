a5c26530c5c11d3f1360dce60de14816ec8054d1
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2017-10-04 00:58:21 +0530

    program to convert hexa from decimal.

---
+++ 21C.C
@@ -0,0 +1,12 @@
+//program to convert octal from decimal
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ int n;
+  clrscr();
+  printf("enter a number\n");
+    scanf("%d",&n);
+    printf("octal equivalent of %d is %o",n,n);
+    getch();
+    return 0;
+    }
