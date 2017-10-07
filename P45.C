a9ab6ad9eeb65f15223da831e501d19e2c9da18f
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2017-10-08 01:08:21 +0530

    program to print pattern .
    here we print the pattern as
    0+9=9
    1+8=9
    .
    .
    .
    9+0=9

---
+++ P45.C
@@ -0,0 +1,12 @@
+//program to print the pattern
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int i,j;
+ clrscr();
+ for(i=0,j=9;j>=0;j--,i++)
+ {printf("%d+%d=%d\n",i,j,i+j);}
+
+   printf("\ncoded by shahnawaz shaikh");
+   getch();
+   return 0;}
