508bf572395c7f2376d1a0facf737ab35bf0af4a
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2017-10-08 01:09:22 +0530

    table of any number
    table of any number as
    17*1=17
    17*2=34
    .
    .
    .
    17*10=170

---
+++ P46.C
@@ -0,0 +1,13 @@
+//program to print the table
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int n,i,j;
+ clrscr();
+ printf("Enter any number to print the table");
+ scanf("%d",&n);
+ for(i=1;i<=10;i++)
+ { printf("%dx%d=%d\n",n,i,n*i);}
+   printf("\ncoded by shahnawaz shaikh");
+   getch();
+   return 0;}
