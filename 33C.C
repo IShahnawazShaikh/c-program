3026f4c50669c8163057f4fea37e77eacbd19501
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2017-10-08 01:06:44 +0530

    programt to swap number

--- 33C.C
+++ 33C.C
@@ -1,16 +1,16 @@
-//program to swap number in 1 line
+//program to swap 2 number using * and / operator
 #include<stdio.h>
 #include<conio.h>
 int main()
-{   int a,b;
-    clrscr();
-    printf("enter two number\n");
-    scanf("%d%d",&a,&b);
-    printf("Before swaping\n");
-    printf("first numeber=%d\nSecond number=%d",a,b);
-    a=(a+b)-(b=a);  //swaping
-    printf("\n\nAfter swaping\n");
-    printf("first numeber=%d\nSecond number=%d",a,b);
-      getch();
-      return 0;
+{ int a,b;
+   clrscr();
+   printf("enter two number\n");
+   scanf("%d%d",&a,&b);
+   printf("\nBefore swaping\nfirst number=%d\nsecond number=%d",a,b);
+   a=a*b;
+   b=a/b;
+   a=a/b;
+   printf("\nAfter swaping\nfirst number=%d\nsecond number=%d",a,b);
+   getch();
+   return 0;
    }
