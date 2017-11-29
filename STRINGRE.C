f5798d574acee75ea0e22d3e131908c26c73e2b1
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2017-11-29 22:17:07 +0530

    program to reverese the string

---
+++ STRINGRE.C
@@ -0,0 +1,41 @@
+/*write a program to print the string in reverse order*/
+/*program made by SHAHNAWAZ*/
+/*roll number 16-DCS-060*/
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{char n[25];
+clrscr();
+puts("\nenter the string\n");
+gets(n);
+strrev(n);
+puts(n);
+getch();
+}
+  //output
+
+enter the string
+
+AIMALSI  AILLIM AIMAJ
+JAMIA MILLIA  ISLAMIA
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
